c1ef67eafdcf789f69f97ba251122775eda1f961
Abhijeet Jagdish Korde <103313466+Abhijeetk09@users.noreply.github.com>
2023-03-07 23:30:41 +0530

    Add files via upload

---
+++ Employee-Management-System/src/main/java/com/demo/model/Employee.java
@@ -0,0 +1,28 @@
+package com.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "EmployeeInfo")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String address;
+	private String email;
+	private String phno;
+	private int salary;
+}
